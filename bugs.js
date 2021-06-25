ad5e5e4a04791d5b537ab227ebcdaa08425a9c70
Sylvester <nnaemekasilver@gmail.com>
2021-06-25 13:04:01 +0100

    Add js with bugs

---
+++ bugs.js
@@ -0,0 +1,37 @@
+
+
+// PRINTING THE CONTENTS OF THE OBJECT
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+persons.size = '50kg';
+person.eyeColor = 'red';
+
+return(persons);
+
+// PRINTING THE FIRST ITEM IN THE PROFILE ARRAY
+let profile = [
+  {
+    name: 'Adonike',
+    age: 50,
+    religion: 'none',
+    choices: true
+  },
+  {
+    name: 'Sylvester',
+    age: 90,
+    religion: 'no idea',
+    choices: true 
+  }
+];
+
+return(profile[3])
+
+// LOOPING THROUGH AND ADDING CONTENTS TO THE CARS VARIABLE
+let records = ['Ford', 'Mercedes', 'Toyota', 'Honda', 'Lamborghini', 'Kia', 'Peugot'];
+
+if(records[ford] === 'Mitsubishi') {
+  return("I love it");
+} else {
+  return("Maybe next time")
+}
